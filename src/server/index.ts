b450973b6b9075dfe825b8e15c945948ddc44187
antrenament <intentart@gmail.com>
2020-01-27 21:46:39 +0000

    Add npm bodyParser and dummy data, and testing a route to delete data

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,28 @@
-// @ts-ignore
 import express from 'express';
+import bodyParser from 'body-parser';
+import { dummyData } from './dummyData';
 
 const app = express()
 const port = 9000
 
-const one: number = 1
-console.log(one)
+app.use(bodyParser.json())
 
-app.get('/', (_req, res) => res.send('Hello World!'))
+//listing your dummy data
+app.get('/dummy', (_req, res) => {
+  return res.send(dummyData)
+})
+// delete listings
+app.post('/delete-dummy', (req, res) => {
+  const id: string = req.body.id
+
+  for (let i = 0; i < dummyData.length; i++) {
+    if (dummyData[i].id === id) {
+      return res.send(dummyData.splice(i, 1))
+    }
+  }
+
+  return res.send('failed to delete dummy');
+})
 
 app.listen(port)
 
