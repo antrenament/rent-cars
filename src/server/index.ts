5584645f4691d4ae804f5a96753ff862309199c3
antrenament <intentart@gmail.com>
2020-01-29 23:38:52 +0000

    Add graphql setup. Refactor index.ts to use ApolloServer + schema

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,28 +1,12 @@
 import express from 'express';
-import bodyParser from 'body-parser';
-import { dummyData } from './dummyData';
+import { ApolloServer } from 'apollo-server-express'
+import { schema } from './graphql'
 
 const app = express()
 const port = 9000
 
-app.use(bodyParser.json())
-
-//listing your dummy data
-app.get('/dummy', (_req, res) => {
-  return res.send(dummyData)
-})
-// delete listings
-app.post('/delete-dummy', (req, res) => {
-  const id: string = req.body.id
-
-  for (let i = 0; i < dummyData.length; i++) {
-    if (dummyData[i].id === id) {
-      return res.send(dummyData.splice(i, 1))
-    }
-  }
-
-  return res.send('failed to delete dummy');
-})
+const server = new ApolloServer({ schema })
+server.applyMiddleware({ app, path: '/api' })
 
 app.listen(port)
 
