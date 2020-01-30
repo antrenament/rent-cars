c626b0e3992a4121caef51ff1e4a1e93962520f5
antrenament <intentart@gmail.com>
2020-01-30 23:45:31 +0000

    Add esm to package.json trying to solve the import problem, although this needs debugging

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,13 +1,13 @@
-import express from 'express';
-import { ApolloServer } from 'apollo-server-express'
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
 import { schema } from './graphql'
 
 const app = express()
-const port = 9000
+const port = 9000;
 
-const server = new ApolloServer({ schema })
-server.applyMiddleware({ app, path: '/api' })
+const server = new ApolloServer({ schema });
+server.applyMiddleware({ app, path: "/api" });
 
-app.listen(port)
+app.listen(port);
 
-console.log(`[app]: https://localhost:${port}`)
+console.log(`[app]: http://localhost:${port}`);
