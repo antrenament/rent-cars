b5c1a9d6ead38ebff4425a8e8757dc15907ab7ce
antrenament <intentart@gmail.com>
2020-02-05 23:19:09 +0000

    Remove cars.ts, Refactor resolvers.ts

--- src/server/graphql/resolvers.ts
+++ src/server/graphql/resolvers.ts
@@ -1,22 +1,28 @@
 import { IResolvers } from 'apollo-server-express'
-import { cars } from '../cars'
-
+import { Database } from '../../lib/types'
+import { ObjectId } from 'mongodb'
 
 export const resolvers: IResolvers = {
   Query: {
-    cars: () => {
-      return cars;
+    cars: async (_root: undefined, _args: {}, { db }: { db: Database }) => {
+      return await db.cars.find({}).toArray()
     }
   },
   Mutation: {
-    deleteCar: (_root: undefined, { id }: { id: string }) => {
-      for (let i = 0; i < cars.length; i++) {
-        if (cars[i].id === id) {
-          return cars.splice(i, 1)[0]
-        }
+    deleteCar: async (
+      _root: undefined,
+      { id }: { id: string },
+      { db }: { db: Database }
+    ) => {
+      const deleteRes = await db.cars.findOneAndDelete({
+        _id: new ObjectId(id)
+      })
+
+      if (!deleteRes.value) {
+        throw new Error('failed to delete cars')
       }
 
-      throw new Error('failed to delete car')
+      return deleteRes.value
     }
   }
 }
