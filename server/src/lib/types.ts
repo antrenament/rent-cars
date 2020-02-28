f3f89f2264ec37f50629c5e5fa62ac763e3f444d
antrenament <intentart@gmail.com>
2020-02-28 23:52:34 +0000

    Fixing small issues for database model

--- server/src/lib/types.ts
+++ server/src/lib/types.ts
@@ -1,14 +1,60 @@
 import { ObjectId, Collection } from 'mongodb'
 
+export enum ListingType {
+  Car = "CAR",
+  Van = "VAN",
+  Truck = "TRUCK"
+}
+
+export interface BookingIndexMonth {
+  [key: string]: Boolean
+}
+
+export interface BookingsIndexYear {
+  [key: string]: BookingIndexMonth;
+}
+
+export interface BookingsIndex {
+  [key: string]: BookingsIndexYear
+}
+
 export interface Booking {
-  _id: ObjectId
+  _id: ObjectId;
+  listing: ObjectId;
+  renter: string;
+  checkIn: string;
+  checkOut: string;
 }
 export interface Listing {
   _id: ObjectId;
+  title: string;
+  description: string;
+  image: string; 
+  host: string;
+  type: ListingType;
+  address: string;
+  country: string;
+  admin: string;
+  city: string;
+  bookings: ObjectId[];
+  model: string;
+  year: number;
+  milage: number;
+  bookingsIndex: BookingsIndex;
+  price: number;
+  seats: number;
 }
 
 export interface User {
   _id: string;
+  token: string;
+  name: string;
+  avatar: string;
+  contact: string;
+  walletId?: string;
+  income: number;
+  bookings: ObjectId[];
+  listings: ObjectId[];
 }
 
 export interface Database {
